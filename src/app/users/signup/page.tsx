039839a37d30288a186ddd5696056f16840d32e6
Angie Rodriguez <angrodri902@gmail.com>
2023-11-26 12:17:34 -0800

    Complete Signup

--- src/app/users/signup/page.tsx
+++ src/app/users/signup/page.tsx
@@ -1,28 +1,66 @@
+"use client";
 import { useForm, SubmitHandler } from "react-hook-form";
+import { createClient } from "@supabase/supabase-js";
 
 type Inputs = {
-  example: string;
+  email: string;
   password: string;
   passwordRequired: string;
 };
+
+const supabase = createClient(
+  `${process.env.NEXT_PUBLIC_SUPABASE_URL}`,
+  `${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}`
+);
+console.log(supabase);
 
 export default function SignUp() {
   const {
     register,
     handleSubmit,
-    watch,
+    getFieldState,
     formState: { errors },
   } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+  const onSubmit: SubmitHandler<Inputs> = async (inputs) => {
+    console.log(inputs);
+    // const email = getFieldState("email");
+    // const password = getFieldState("password")
+
+    const { data, error } = await supabase.auth.signUp({
+      email: inputs.email,
+      password: inputs.password,
+      options: {
+        emailRedirectTo: "http://localhost:3000/welcome",
+      },
+    });
+
+    // if (data) {
+    //   fetch("http://localhost:3000/welcome", (req, res) => {
+
+    //   })
+    // } else if (error) {
+
+    // }
+
+    console.log(data);
+  };
 
   return (
     /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
     <form onSubmit={handleSubmit(onSubmit)}>
       {/* register your input into the hook by invoking the "register" function */}
-      <input defaultValue="test" {...register("example")} />
+      <input placeholder="test" {...register("email", { required: true })} />
+      {errors.email && <span>This field is required</span>}
 
       {/* include validation with required or other standard HTML validation rules */}
-      <input {...register("passwordRequired", { required: true })} />
+      <input type="password" {...register("password", { required: true })} />
+      {/* errors will return when field validation fails  */}
+      {errors.password && <span>This field is required</span>}
+
+      <input
+        type="password"
+        {...register("passwordRequired", { required: true })}
+      />
       {/* errors will return when field validation fails  */}
       {errors.passwordRequired && <span>This field is required</span>}
 
