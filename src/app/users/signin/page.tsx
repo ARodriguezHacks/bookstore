82f26f71b571066df2e66f13737bace2d546023f
Angie Rodriguez <angrodri902@gmail.com>
2023-11-28 11:49:42 -0800

    Initialize signin functionality

--- src/app/users/signin/page.tsx
+++ src/app/users/signin/page.tsx
@@ -1,5 +1,45 @@
+"use client";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { supabase } from "../../../../lib/initSupabase";
+
+type Inputs = {
+  email: string;
+  password: string;
+};
+
 export default function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = async (inputs) => {
+    console.log(inputs);
+
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: inputs.email,
+      password: inputs.password,
+    });
+
+    console.log(data);
+  };
+
   return (
-    <h1>Sign In</h1>
-  )
+    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
+    <>
+      <h1>Sign In</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {/* register your input into the hook by invoking the "register" function */}
+        <input placeholder="test" {...register("email", { required: true })} />
+        {errors.email && <span>This field is required</span>}
+
+        {/* include validation with required or other standard HTML validation rules */}
+        <input type="password" {...register("password", { required: true })} />
+        {/* errors will return when field validation fails  */}
+        {errors.password && <span>This field is required</span>}
+
+        <input type="submit" />
+      </form>
+    </>
+  );
 }
--- src/app/users/signup/page.tsx
+++ src/app/users/signup/page.tsx
@@ -1,18 +1,12 @@
 "use client";
 import { useForm, SubmitHandler } from "react-hook-form";
-import { createClient } from "@supabase/supabase-js";
+import { supabase } from "../../../../lib/initSupabase";
 
 type Inputs = {
   email: string;
   password: string;
   passwordRequired: string;
 };
-
-const supabase = createClient(
-  `${process.env.NEXT_PUBLIC_SUPABASE_URL}`,
-  `${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}`
-);
-console.log(supabase);
 
 export default function SignUp() {
   const {
